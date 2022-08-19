cce148a70f96b48b26502b1106f9842131cd48aa
Frida <frida34estefania@gmail.com>
2022-08-19 15:47:39 -0500

    “Outputs”

--- documentation/image1.py
+++ documentation/image1.py
@@ -16,13 +16,13 @@ import argparse
 
 # Constants, these are the main "settings" for the image
 WIDTH, HEIGHT, MARGIN, FRAMES = 2048, 2048, 128, 1
-FONT_PATH = "fonts/ttf/Rubik-Regular.ttf"
+FONT_PATH = "fonts/ttf/Kalnia-Color-Bold.ttf"
 FONT_LICENSE = "OFL v1.1"
 AUXILIARY_FONT = "Helvetica"
 AUXILIARY_FONT_SIZE = 48
 BIG_TEXT = "Aa"
 BIG_TEXT_FONT_SIZE = 1024
-BIG_TEXT_SIDE_MARGIN = MARGIN * 3.1
+BIG_TEXT_SIDE_MARGIN = MARGIN * 2.1
 BIG_TEXT_BOTTOM_MARGIN = MARGIN * 5.5
 GRID_VIEW = False # Change this to "True" for a grid overlay
 
