7789d6eb8ee04bf90fca2eea0bf94dba7d1c8a7c
Frida Medrano <frida34estefania@gmail.com>
2024-03-03 16:36:52 -0800

    update on paints, folders and files naming, update on icons

--- sources/paints.py
+++ sources/paints.py
@@ -2,8 +2,8 @@ from collections import defaultdict
 import re
 from fontTools.pens.boundsPen import BoundsPen
 
-ALPHA_AXIS_WDTH = { (("wdth", 100),): 0.0, (("wdth", 115),): 1.0, (("wdth", 125),): 0.0 }
-ALPHA_AXIS_WGHT = { (("wght", 100),): 1.0, (("wght", 100),): 0.0, (("wght", 700),): 1.0 }
+ALPHA_AXIS_WDTH = { (("wdth", 100),): 1.0, (("wdth", 105),): 1.0, (("wdth", 110),): 0.0, (("wdth", 115),): 1.0, (("wdth", 120),): 0.0 (("wdth", 125),): 1.0 }
+ALPHA_AXIS_WGHT = { (("wght", 100),): 1.0, (("wght", 250),): 0.0, (("wght", 400),): 1.0, (("wght", 550),): 0.0, (("wght", 700),): 1.0 }
 ALPHA_AXIS_NONE = { (("wdth", 100),): 1.0, (("wdth", 125),): 1.0 }
 
 MG01 = { (("wght", 100),): 0.0, (("wght", 700),): 0.2 }
@@ -14,8 +14,9 @@ MG05 = { (("wght", 100),): 0.8, (("wght", 700),): 1.0 }
 
 SG01 = { (("wght", 100),): 0.0, (("wght", 700),): 0.1 }
 SG02 = { (("wght", 100),): 0.1, (("wght", 700),): 0.2 }
-SG03 = { (("wght", 100),): 0.2, (("wght", 700),): 0.8 }
-SG04 = { (("wght", 100),): 0.8, (("wght", 700),): 1.0 }
+SG03 = { (("wght", 100),): 0.3, (("wght", 700),): 0.4 }
+SG04 = { (("wght", 100),): 0.6, (("wght", 700),): 0.8 }
+SG05 = { (("wght", 100),): 0.9, (("wght", 700),): 1.0 }
 
 
 base_glyphs = []
@@ -45,10 +46,10 @@ for glyphname in base_glyphs:
     GRADIENT1 = PaintLinearGradient(
     (xMin, yMin), (xMax, yMax), (-90, 100),
     ColorLine([
-        (MG01, "#F67F68FF"),
-        (MG02, ("#FDE7D8FF", ALPHA_AXIS_NONE )),
+        (MG01, "#FFB6A6FF"),
+        (MG02, ("#FFFFFFFF", ALPHA_AXIS_NONE )),
         (MG03, "#FF8B8BFF"),
-        (MG04, "#FDE7D8FF"),
+        (MG04, "#FFFFFFFF"),
         (MG05, "#7AA0FFFF")
         ])
     )
@@ -58,8 +59,9 @@ for glyphname in base_glyphs:
     ColorLine([
         (SG01, ("#FFB5C2FF", ALPHA_AXIS_WGHT )),
         (SG02, ("#EAD5D3FF", ALPHA_AXIS_WDTH )),
-        (SG03, ("#FFB6A6FF", ALPHA_AXIS_WGHT )),
-        (SG04, ("#D6E4EEFF", ALPHA_AXIS_WDTH )),
+        (SG03, ("#F9C4BAFF", ALPHA_AXIS_NONE )),
+        (SG04, ("#FFB6A6FF", ALPHA_AXIS_WDTH )),
+        (SG05, ("#D6E4EEFF", ALPHA_AXIS_WGHT )),
         ])
     )
 
